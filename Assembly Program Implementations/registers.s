d3f85f57db3972e217ac9ea72e2a9b0f6bc58619
Rick Truong <rickvtruong@gmail.com>
2023-03-26 00:03:45 -0700

    updated registers.s

--- Assembly Program Implementations/registers.s
+++ Assembly Program Implementations/registers.s
@@ -1,12 +1,12 @@
 ; REGISTER FILE VALUES
 $0 = 0                  ; (i = 0)
-$1 =  b9   0   0   0 _   0   0   0 b11
-$2 =   0   0   0   0 _   0   0  b7   0
+$1 =  b9   0   0   0 _  b4  b5  b7 b11
+$2 =   0   0   0   0 _  b4   0   0   0
 $3 =  b4  b3  b2  p4 _  b1  p2   0   0
 
-$4 =  b9   0   0   0 _   0   0   0   0
-$5 = 1
+$4 =   0   0   0   0 _  b4   0   0   0
+$5 = 3
 $6 =  b4  b3  b2  p4 _  b1  p2   0   0
 $7 = b11 b10  b9  b8 _  b7  b6  b5  p8
 
-; REGISTER FILE AS OF LINE 130 PROG_1.S
+; REGISTER FILE AS OF LINE 152 prog_1.s and PC = 106
