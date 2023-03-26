3e6983f789c14dc3f5d52fa73a6713640b525ae5
Rick Truong <rickvtruong@gmail.com>
2023-03-25 22:30:47 -0700

    Including notes: P8, P4, P2 debugging seemingly complete, P1 and P0 seemingly buggy

--- Assembly Program Implementations/registers.s
+++ Assembly Program Implementations/registers.s
@@ -1,4 +1,4 @@
-; REGISTER FILE NOTES
+; REGISTER FILE VALUES
 $0 = 0                  ; (i = 0)
 $1 =  b9   0   0   0 _   0   0   0 b11
 $2 =   0   0   0   0 _   0   0  b7   0
@@ -8,3 +8,5 @@ $4 =  b9   0   0   0 _   0   0   0   0
 $5 = 1
 $6 =  b4  b3  b2  p4 _  b1  p2   0   0
 $7 = b11 b10  b9  b8 _  b7  b6  b5  p8
+
+; REGISTER FILE AS OF LINE 130 PROG_1.S
